094d6717ec57cdf4a69c78d4ca0e614e8e1546ec
Emilio <emiliodev1986@gmail.com>
2022-06-08 10:53:25 -0400

    making SnackVoting Responsive

--- src/components/SnackVoting.tsx
+++ src/components/SnackVoting.tsx
@@ -8,6 +8,7 @@ const itemsList = [
   { name: "Clif Chocolate Chip Energy Bar", amount: 8 },
   { name: "Cheetos Chips", amount: 12 },
   { name: "Baked Lays Potato Chips", amount: 4 },
+  { name: "Hersheys Milk Chocolate", amount: 5 },
   { name: "Jack Links Beef Jerky", amount: 8 },
   { name: "Planters Salted Peanuts", amount: 2 },
 ];
@@ -48,20 +49,30 @@ const SnackVoting = () => {
           ))}
         </div>
         {/* selection items here */}
-        <div className="snack-selection-items">
-            <div className="snack-tb-info selection">
-              <h4 className="hdg hdg_4  snack-avilable-item section">
-                Selection
-              </h4>
-              <div className="snack-baged">
-                <span className="snack-baged-num">{3}</span>
-              </div>
+        <div className="snack-avilable-items selection">
+          <div className="snack-tb-info selection">
+            <h4 className="hdg hdg_4 mix-hdg_dark snack-avilable-item selection">
+              Selection
+            </h4>
+            <div className="snack-baged selection">
+              <span className="snack-baged-num selection">
+                {itemsList.length}
+              </span>
+            </div>
           </div>
-
-          <div>selection</div>
-          <div>selection</div>
-          <div>selection</div>
-          <div>selection</div>
+          {/* here iterable content */}
+          {itemsList.map((item) => (
+            <div className="snack-tb-item selection" key={item.name}>
+              <div className="snack-tb-item snack_name selection">
+                <h6 className="hdg hdg_6 mix-hdg_dark snack-avilable-item selection">
+                  {item.name}
+                </h6>
+                <div className="snack-baged snack_amount">
+                  <span className="snack-baged-num">{item.amount}</span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
